64ea27ed34ad462617471926ec62a6880c4b867b
AndreAle94 <motardx94@gmail.com>
2019-09-29 18:55:23 +0200

    [OSM] Fixed bug when loading tiles
    - fixed bug related to read-only filesystem when reading cached tiles
    - improved UI when using OpenStreetMap build flavor
    - upgraded compile and target SDK to version 29

--- app/src/osm/java/com/oriondev/moneywallet/view/MapViewWrapper.java
+++ app/src/osm/java/com/oriondev/moneywallet/view/MapViewWrapper.java
@@ -19,20 +19,29 @@
 
 package com.oriondev.moneywallet.view;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.oriondev.moneywallet.R;
 import com.oriondev.moneywallet.model.Coordinates;
 import com.oriondev.moneywallet.model.Place;
 
 import org.osmdroid.api.IGeoPoint;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.config.IConfigurationProvider;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.CopyrightOverlay;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
 import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.util.constants.OverlayConstants;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,6 +50,8 @@ import java.util.List;
  */
 
 public class MapViewWrapper {
+
+    private static final String TILE_CACHE_FOLDER = "osm_tiles";
 
     private static final double DEFAULT_MIN_ZOOM_LEVEL = 4d;
     private static final double DEFAULT_ZOOM_LEVEL = 14d;
@@ -56,10 +67,34 @@ public class MapViewWrapper {
         mMapView.setMultiTouchControls(true);
         mMapView.setMinZoomLevel(DEFAULT_MIN_ZOOM_LEVEL);
         mMapView.getController().setZoom(DEFAULT_ZOOM_LEVEL);
+        setupConfiguration(mMapView.getContext());
+    }
+
+    private void setupConfiguration(Context context) {
+        IConfigurationProvider configuration = Configuration.getInstance();
+        // to prevent using a default user agent, we can set it to the current package name
+        configuration.setUserAgentValue(context.getPackageName());
+        // fix for empty map caused by a read-only access to the file system: to overcame this issue
+        // we can use a temp directory inside the cache folder of the application
+        File cacheFolder = context.getExternalCacheDir();
+        if (cacheFolder != null) {
+            // we have a reference to the global cache folder of the application, maybe it's better
+            // to create a sub folder for caching tiles
+            File tileCache = new File(cacheFolder, TILE_CACHE_FOLDER);
+            if (tileCache.exists() || tileCache.mkdir()) {
+                configuration.setOsmdroidTileCache(tileCache);
+            }
+        }
+    }
+
+    private void setupCopyrightOverlay() {
+        // OpenStreetMap requires that you add “© OpenStreetMap contributors” to the map
+        mMapView.getOverlayManager().add(0, new CopyrightOverlay(mMapView.getContext()));
     }
 
     public void onCreate(Bundle savedInstanceState) {
-        // do nothing
+        // here we can initialize the copyright overlay
+        setupCopyrightOverlay();
     }
 
     public void onStart() {
@@ -111,6 +146,10 @@ public class MapViewWrapper {
         });
     }
 
+    private Drawable getMarkerIcon() {
+        return mMapView.getContext().getResources().getDrawable(R.drawable.ic_osm_marker);
+    }
+
     public void addPlaces(List<Place> places) {
         if (mMapView != null && places != null && !places.isEmpty()) {
             int pointCount = 0;
@@ -130,12 +169,12 @@ public class MapViewWrapper {
                     centerLongitude += coordinates.getLongitude();
                 }
             }
-            ItemizedOverlayWithFocus<OverlayItem> overlay = new ItemizedOverlayWithFocus<>(mMapView.getContext(), items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            ItemizedOverlayWithFocus<OverlayItem> overlay = new ItemizedOverlayWithFocus<>(items, getMarkerIcon(), getMarkerIcon(), OverlayConstants.NOT_SET, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
 
                 @Override
                 public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
                     if (mInfoWindowClickListener != null && item.getUid() != null) {
-                        Long parsedId = Long.parseLong(item.getUid());
+                        long parsedId = Long.parseLong(item.getUid());
                         mInfoWindowClickListener.onInfoWindowClick(parsedId);
                     }
                     return true;
@@ -146,8 +185,15 @@ public class MapViewWrapper {
                     return false;
                 }
 
-            });
+            }, mMapView.getContext());
             overlay.setFocusItemsOnTap(true);
+            overlay.setMarkerTitleForegroundColor(Color.BLACK);
+            overlay.setMarkerDescriptionForegroundColor(Color.BLACK);
+            overlay.setMarkerBackgroundColor(Color.WHITE);
+
+            overlay.setDescriptionBoxPadding(10);
+            overlay.setDescriptionBoxCornerWidth(10);
+
             mMapView.getOverlays().add(overlay);
             // setup the map-view to better handle multiple locations
             if (pointCount > 0) {
@@ -165,7 +211,7 @@ public class MapViewWrapper {
             GeoPoint geoPoint = new GeoPoint(coordinates.getLatitude(), coordinates.getLongitude());
             List<OverlayItem> items = new ArrayList<>();
             items.add(new OverlayItem("", "", geoPoint));
-            ItemizedOverlayWithFocus<OverlayItem> overlay = new ItemizedOverlayWithFocus<>(mMapView.getContext(), items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            ItemizedOverlayWithFocus<OverlayItem> overlay = new ItemizedOverlayWithFocus<>(items, getMarkerIcon(), null, OverlayConstants.NOT_SET, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
 
                 @Override
                 public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
@@ -177,7 +223,7 @@ public class MapViewWrapper {
                     return false;
                 }
 
-            });
+            }, mMapView.getContext());
             overlay.setFocusItemsOnTap(true);
             mMapView.getOverlays().add(overlay);
             // setup map-view using current point as center
@@ -188,6 +234,7 @@ public class MapViewWrapper {
 
     public void clear() {
         mMapView.getOverlays().clear();
+        setupCopyrightOverlay();
     }
 
     public void setOnInfoClickListener(OnInfoWindowClickListener callback) {
